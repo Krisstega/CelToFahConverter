de70fe8c7a0cf4927bfa1e20bf34af946e7863ac
Oghenetega Ewhubare <christega94@gmail.com>
2022-09-07 02:02:43 -0700

    initial commit

---
+++ CelsiusConv/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CelsiusConv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius;
+            Console.WriteLine(" Please enter your temperature in celsius:");
+            celsius = Convert.ToDouble(Console.ReadLine());
+
+            CelsConv fah = new CelsConv(celsius);
+            fah.ConvertToFah();
+
+            Console.ReadLine();
+        }
+    }
+
+    class CelsConv
+    {
+        double celsius;
+        public CelsConv( double celsius)
+        {
+            this.celsius = celsius;
+
+        }
+        public void ConvertToFah()
+        {
+            double fah = (celsius * 1.8) + 32;
+            Console.WriteLine("temperature in {0}  celsuis = {1} in fahrenheit", celsius, fah);
+
+        }
+    }
+}
